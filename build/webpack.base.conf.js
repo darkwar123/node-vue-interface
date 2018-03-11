41f59b6c5c8047962bff52911453642763400427
Said Gadzhiev <darkwar123@users.noreply.github.com>
2018-03-11 17:08:53 +0300

    Add favicon support

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -72,5 +73,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new FaviconsWebpackPlugin('./src/assets/images/favicon.png')
+  ],
 }
